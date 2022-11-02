df3ed26977fdd9d31d5ec02d7179ad2a6abca4db
Proliix <lucas.andreasson@hotmail.com>
2022-11-02 14:33:52 +0100

    ToothpasteTub fixes

--- CloseGameJam/Assets/Scripts/TubeController.cs
+++ CloseGameJam/Assets/Scripts/TubeController.cs
@@ -6,12 +6,16 @@ public class TubeController : MonoBehaviour
 {
     public GameObject ToothPaste;
     public Transform SpawnPos;
+    public Sprite NormalTube;
+    public Sprite squishedTube;
 
+    private SpriteRenderer sr;
     private GameObject player;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
 
@@ -19,8 +23,17 @@ public class TubeController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player") && player.transform.position.y > gameObject.transform.position.y)
         {
+            sr.sprite = squishedTube;
             GameObject obj = Instantiate(ToothPaste, SpawnPos.position, ToothPaste.transform.rotation);
             Destroy(obj, 5);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            sr.sprite = NormalTube;
         }
     }
 }
